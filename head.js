37c9b9825d66ec156129d7e9bb884e28d35c395b
vagrant <jabrilgadhane@hotmail.ca>
2021-11-23 03:36:04 +0000

    first commit

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log("✅ ✅ ✅   Assertion passed: ${actual} === ${expected}");
+    } else {
+      console.log("🛑 🛑 🛑  Assertion failed: ${actual} !== ${expected}");
+    }
+  };
+    
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
+  assertEqual(head([5,6,7]),5);
+   
+ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
