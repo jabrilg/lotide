7827810849947555a5dae93e1d7417efa5d27a87
vagrant <jabrilgadhane@hotmail.ca>
2021-11-24 22:27:24 +0000

    first commit

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = require("./assertEqual");
+
+const eqArrays = function(actual, expected) {
+  let check = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      check = false;
+    }
+  }
+
+  if (actual.length !== expected.length) {
+    check = false;
+  }
+
+  return check;
+};
+
+module.exports = eqArrays; 
