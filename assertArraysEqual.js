9eba6a1fb3d2c42437f9c017161f06111f38b6b8
vagrant <jabrilgadhane@hotmail.ca>
2021-11-24 22:32:09 +0000

    first commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected) === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+module.exports = assertArraysEqual;
