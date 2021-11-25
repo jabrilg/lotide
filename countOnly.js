ad508c5a8e49c5ca96e1080257aef0a1aafde837
vagrant <jabrilgadhane@hotmail.ca>
2021-11-25 01:13:21 +0000

    first commit

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      return console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  // allItems: an array of strings that we need to look through
+  // itemsToCount: an object specifying what to count
+  const countOnly = function(allItems, itemsToCount) {
+    let obj = {};
+    for (let val of allItems) {
+      if (itemsToCount[val]) {
+        if (obj[val]) {
+          obj[val] += 1;
+        } else {
+          obj[val] = 1;
+        }
+      }
+    }
+    return obj;
+  };
+  
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  console.log(result1)
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
+  assertEqual(result1["Agouhanna"], undefined);
